766db1363e53d2f35d667e3569e1ef926f61dd64
HippiHo <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>
2025-01-18 13:00:21 +0100

    Adds usercards and count chart to admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,32 @@
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
-  return <div className="">AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* left */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* user cards */}
+        <div className="flex justify-between gap-4 flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        {/* middle charts */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* count chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+        </div>
+        {/*  bottom chart */}
+        <div className="w-full h-[500px]"></div>
+      </div>
+      {/* right */}
+      <div className="w-full lg:w-1/3"></div>
+    </div>
+  );
 };
 
 export default AdminPage;
